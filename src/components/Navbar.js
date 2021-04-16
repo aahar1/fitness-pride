4eb77b03489e7a58c209d21006336503c3ce5a79
Aaron Harris <aaron.harris711@gmail.com>
2021-04-15 22:49:01 -0400

    css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,7 +18,7 @@ class Navbar extends Component {
                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
 
                </div>
-               <ul className={this.state.clicked ? 'nav-menu active' : 'fas fa-bars'}>
+               <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
                    {MenuItems.map((item, index) => {
                        return(
                            <li key={index}>
