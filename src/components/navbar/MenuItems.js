1bd45626273d675717693e11be739a9918c15765
MICHAEL <MJENNINGS6@gmail.com>
2021-06-27 17:12:43 -0400

    created navbar folder in components to consolidate navbar itmes

---
+++ src/components/navbar/MenuItems.js
@@ -0,0 +1,28 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url:'#',
+        cName: 'nav-links'
+    },
+
+    {
+        title: 'Trainers',
+        url:'#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Bodybuilding Events',
+        url:'#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'About Us',
+        url:'#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sign up',
+        url:'#',
+        cName: 'nav-links'
+    },
+]
