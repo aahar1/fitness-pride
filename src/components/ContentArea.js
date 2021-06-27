4bc42ca5fcce261c1a2b1b45c9a5ba176091c2e1
Aaron Harris <aaron.harris711@gmail.com>
2021-06-27 17:25:44 -0400

    added padding to content area

--- src/components/ContentArea.js
+++ src/components/ContentArea.js
@@ -1,22 +1,27 @@
 import React from 'react'
+import './ContentArea.css';
 
 function ContentArea() {
     return (
         <div className='content-container'>
-            <video src='/public/posing-video.mp4'
-                autoPlay loop muted />
+            {/* <video src='/public/posing-video.mp4'
+                autoPlay loop muted /> */}
                 <h1>BE STRONG! BE BRAVE! BE U!</h1>
             <p>Lorem Ipsum is simply dummy
                  text of the printing and 
                  typesetting industry. 
                      essentially unchanged.</p>
+            <p>Lorem Ipsum is simply dummy
+                 text of the printing and 
+                 typesetting industry. 
+                     essentially unchanged.</p>
           <div className='content-btns'>
-            <button className='btns' 
+            {/* <button className='btns' 
                     buttonStyle='btn--outline'
                     buttonSize='btn--large'
             >
                 Fitness junkies enter Here
-             </button>
+             </button> */}
           </div>
 
         </div>
