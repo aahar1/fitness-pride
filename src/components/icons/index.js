b4a7b962d35c2fe60db8b0d639d43dcd205b741d
MICHAEL <MJENNINGS6@gmail.com>
2021-06-30 10:23:10 -0400

    syntax fix

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,6 +1,6 @@
-import react from 'react';
-import { Icon } from './styles/icons';
+import React from "react";
+import { Icon } from "./styles/icons";
 
-export default function Icons({className}) {
-    return <Icon className={className} />
+export default function Icons({ className }) {
+  return <Icon className={className} />;
 }
